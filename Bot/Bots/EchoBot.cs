22a2797b8fabf4a0df8f4a74b2428f8f5035c4cc
Konstantin2702 <klaptev15337@gmail.com>
2020-10-10 20:01:55 +0300

    Small changes

--- Bot/Bots/EchoBot.cs
+++ Bot/Bots/EchoBot.cs
@@ -13,20 +13,65 @@ namespace Microsoft.BotBuilderSamples.Bots
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var replyText = $"Echo: {turnContext.Activity.Text}";
-            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+            var reply = MessageFactory.Text("Nothing done?");
+           
+
+            reply.SuggestedActions = new SuggestedActions()
+            {
+                Actions = new List<CardAction>()
+                {
+                    new CardAction() { Title = "1", Type = ActionTypes.ImBack,Value = "1" },
+                    new CardAction() { Title = "2", Type = ActionTypes.ImBack, Value = "2" },
+                    new CardAction() { Title = "3", Type = ActionTypes.ImBack,Value = "3" },
+                }
+            };
+            switch (turnContext.Activity.Text)
+            {
+                case "1":
+                    break;
+                default:
+                    break;
+            }
+                await turnContext.SendActivityAsync(reply, cancellationToken);
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hello and welcome!";
+            /*var welcomeText = "Доброго времени суток, выберите сложность задачи, которую вы хотите решить";
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
                     await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
                 }
+            }*/
+            var reply = MessageFactory.Text("Доброго времени суток, выберите сложность задачи, которую вы хотите решить");
+            reply.SuggestedActions = new SuggestedActions()
+            {
+                Actions = new List<CardAction>()
+                {
+                    new CardAction() { Title = "1", Type = ActionTypes.ImBack, Value = "1"},
+                    new CardAction() { Title = "2", Type = ActionTypes.ImBack, Value = "2" },
+                    new CardAction() { Title = "3", Type = ActionTypes.ImBack, Value = "3" },
+                },
+            };
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
+            private static async Task SendSuggestedActionsAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+            {
+                var reply = MessageFactory.Text("Доброго времени суток, выберите сложность задачи, которую вы хотите решить");
+                reply.SuggestedActions = new SuggestedActions()
+                {
+                    Actions = new List<CardAction>()
+                {
+                    new CardAction() { Title = "1", Type = ActionTypes.ImBack, Value = "1"},
+                    new CardAction() { Title = "2", Type = ActionTypes.ImBack, Value = "2" },
+                    new CardAction() { Title = "3", Type = ActionTypes.ImBack, Value = "3" },
+                },
+                };
+                await turnContext.SendActivityAsync(reply, cancellationToken);
             }
         }
     }
-}
+
+
